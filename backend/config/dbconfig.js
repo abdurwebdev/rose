02328165dabfa4f4996044adc0bfb0cf0ffcc2a2
abdurwebdev <iabdurrehman22.0@gmail.com>
2025-07-17 13:17:09 +0500

    gggg

--- backend/config/dbconfig.js
+++ backend/config/dbconfig.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const dbconfig = () => {
-    mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("✅ Connected to MongoDB"))
-    .catch((err) => {
-        console.error("❌ MongoDB connection error:", err.message);
+const dbconfig = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "abdurwebdev2",
+      useNewUrlParser: true,
+      useUnifiedTopology: true, // this is now ignored but doesn't cause harm
     });
+    console.log("✅ MongoDB connected successfully");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+  }
 };
 
 module.exports = dbconfig;
